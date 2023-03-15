8bfcff55604fcde713ef827ff954a5a17ab9ae15
Denis Chernov <gitlab@dnch.me>
2023-03-14 20:47:42 -0700

    mock web3, requirements.txt

--- server.py
+++ server.py
@@ -4,12 +4,12 @@ Simple API server that returns balance for ETH and USDC wallets.
 import json
 import os
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from web3 import Web3
 
 from error_messages import ErrorMessage
 
-provider = os.getenv("PROVIDER_URL")
+provider = os.getenv("PROVIDER_URL", "http://localhost:8545")
 usdc_contract_address = os.getenv("USDC_CONTRACT_ADDRESS")
 app = FastAPI()
 w3 = Web3(Web3.HTTPProvider(provider))
@@ -20,33 +20,60 @@ contract = w3.eth.contract(
 )
 
 
+def get_eth_balance(address: str):  # pragma: no cover
+    """
+    Return ETH balance from ETH node
+    """
+    return w3.eth.get_balance(Web3.toChecksumAddress(address))
+
+
+def get_usdc_balance(address: str):  # pragma: no cover
+    """
+    Return USDC balance from ETH node
+    """
+    return contract.functions.balanceOf(Web3.toChecksumAddress(address)).call()
+
+
+def get_w3_connected():  # pragma: no cover
+    """
+    Return Web3 connection status
+    """
+    return w3.isConnected()
+
+
 @app.get("/balance/{crypto}/{address}")
-async def eth_balance(crypto: str, address: str):
+def get_balance(
+    crypto: str,
+    address: str,
+    is_connected: bool = Depends(get_w3_connected),
+    eth_balance: str = Depends(get_eth_balance),
+    usdc_balance: str = Depends(get_usdc_balance),
+):
     """
     GET /balance/crypto/address
     """
-    if not w3.isConnected():
+    if not is_connected:
         raise HTTPException(status_code=503, detail=ErrorMessage.NODE_UNAVAILABLE.value)
-    if crypto not in ["eth", "usdc"]:
-        raise HTTPException(status_code=400, detail=ErrorMessage.INVALID_CRYPTO.value)
     try:
-        valid_address = Web3.toChecksumAddress(address)
+        Web3.toChecksumAddress(address)
     except ValueError as error:
         raise HTTPException(
             status_code=400, detail=ErrorMessage.INVALID_ADDRESS.value
         ) from error
     if crypto == "eth":
-        wei = w3.eth.get_balance(valid_address)
-        balance = w3.fromWei(wei, "ether")
+        wei = eth_balance
+        balance = Web3.fromWei(wei, "ether")
     elif crypto == "usdc":
-        wei = contract.functions.balanceOf(Web3.toChecksumAddress(valid_address)).call()
-        balance = w3.fromWei(wei, "mwei")
+        wei = usdc_balance
+        balance = Web3.fromWei(wei, "mwei")
+    else:
+        raise HTTPException(status_code=400, detail=ErrorMessage.INVALID_CRYPTO.value)
     return {"crypto": crypto, "balance": balance}
 
 
 @app.get("/status")
-async def status():
+def status(is_connected: bool = Depends(get_w3_connected)):
     """
-    Status endpoint.
+    web3 connection status
     """
-    return {"status": "Still alive!"}
+    return {"connected": is_connected}
--- test_server.py
+++ test_server.py
@@ -3,13 +3,47 @@ Unit tests for API server.
 """
 from fastapi.testclient import TestClient
 
+from web3 import Web3
 from error_messages import ErrorMessage
-from server import app
+from server import app, get_eth_balance, get_usdc_balance, get_w3_connected
 
 VALID_ADDRESS = "0x300045c41b5334772C25196ac0035bCDD511a821"
-ETH_VALUE = 0.024925767330357064
+ETH_VALUE = 99.99125346475
 USDC_VALUE = 61.280028
 client = TestClient(app)
+
+
+def mocked_get_eth_balance():
+    """
+    Return mocked ETH balance
+    """
+    return Web3.toWei(ETH_VALUE, "ether")
+
+
+def mocked_get_usdc_balance():
+    """
+    Return mocked USDC balance
+    """
+    return Web3.toWei(USDC_VALUE, "mwei")
+
+
+def mocked_w3_connected():
+    """
+    Mock Web3 node status as connected
+    """
+    return True
+
+
+def mocked_w3_disconnected():
+    """
+    Mock Web3 node status as disconnected
+    """
+    return False
+
+
+app.dependency_overrides[get_eth_balance] = mocked_get_eth_balance
+app.dependency_overrides[get_usdc_balance] = mocked_get_usdc_balance
+app.dependency_overrides[get_w3_connected] = mocked_w3_connected
 
 
 class TestServer:
@@ -49,10 +83,28 @@ class TestServer:
         assert response.status_code == 400
         assert response.json() == {"detail": ErrorMessage.INVALID_ADDRESS.value}
 
-    def test_status(self):
+    def test_status_connected(self):
         """
-        Check status endpoint
+        Check status endpoint with connected ETH node
         """
         response = client.get("/status")
         assert response.status_code == 200
-        assert response.json() == {"status": "Still alive!"}
+        assert response.json() == {"connected": True}
+
+    def test_status_disconnected(self):
+        """
+        Check status endpoint with disconnected ETH node
+        """
+        app.dependency_overrides[get_w3_connected] = mocked_w3_disconnected
+        response = client.get("/status")
+        assert response.status_code == 200
+        assert response.json() == {"connected": False}
+
+    def test_balance_disconnected(self):
+        """
+        Check balance endpoint with disconnected ETH node
+        """
+        app.dependency_overrides[get_w3_connected] = mocked_w3_disconnected
+        response = client.get(f"/balance/eth/{VALID_ADDRESS}")
+        assert response.status_code == 503
+        assert response.json() == {"detail": ErrorMessage.NODE_UNAVAILABLE.value}
